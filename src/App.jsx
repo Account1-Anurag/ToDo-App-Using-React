075d8fbfd688f9641b90805cbf82ac2ae574bd83
Anurag <0902cs221015@rjit.ac.in>
2025-04-03 17:47:41 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
 import './App.css'
-import LudoComponent from './LudoComponent'
-import LudoGame from './LudoGame'
 import TodoList from './ToDoList'
 function App() {
 
   return (
     <>
-      {/* <LudoComponent color="blue"/>
-      <LudoComponent color="yellow"/>
-      <LudoComponent color="green"/>
-      <LudoComponent color="red"/>
-      <LudoComponent color="teal"/> */}
-      {/* <LudoGame/>  */}
       <TodoList/>
-
-      
     </>
   )
 }
